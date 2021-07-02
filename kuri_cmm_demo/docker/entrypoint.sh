7705c34aa8a873a6257428ab516a00813591abb0
Nick Walker <nswalker@cs.washington.edu>
2021-07-02 08:57:15 -0700

    Update launch process
    Docker service should be available on boot now
    Check for configuration elements on container entry

--- kuri_cmm_demo/docker/entrypoint.sh
+++ kuri_cmm_demo/docker/entrypoint.sh
@@ -5,4 +5,11 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 export ROS_MASTER_URI=http://127.0.0.1:11311
 export ROS_IP=127.0.0.01
 unset ROS_HOSTNAME
+if [[ -e workspace/env.sh ]]; then
+  source workspace/env.sh
+fi
+
+if [[ -e workspace/devel/setup.bash ]]; then
+  source workspace/devel/setup.bash
+fi
 exec "$@"
--- kuri_cmm_demo/scripts/run_robot.sh
+++ kuri_cmm_demo/scripts/run_robot.sh
@@ -5,5 +5,5 @@ tmux rename-window 'Demo'
 tmux select-window -t demo:0
 tmux send-keys 'source ~/workspaces/nick_ws/devel/setup.bash && source ~/env.sh && roslaunch kuri_launch kuri.launch use_navigation:=false use_transcription:=false use_vision:=false' Enter
 tmux split-window -h -t 1
-tmux send-keys 'sudo service docker start || sudo docker run --rm --network host -v /home/mayfield/workspaces/nick_melodic_ws:/workspace -v /home/mayfield/.aws:/root/.aws -v /mayfield:/mayfield -it demo' Enter
+tmux send-keys 'sudo docker run --rm --network host -v /home/mayfield/workspaces/nick_melodic_ws:/workspace -v /home/mayfield/.aws:/root/.aws -v /mayfield:/mayfield -it demo' Enter
 tmux -2 attach-session -t demo
